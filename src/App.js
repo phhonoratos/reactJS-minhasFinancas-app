cdfa5887e1c81a1fda099f486b7afe44f9611717
Paulo Honorato <paulohonoratos@hotmail.com>
2021-08-05 20:59:33 -0300

    Criação da página de cadastro de usuário

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootswatch/dist/flatly/bootstrap.css'
 import './custom.css'
+import CadastroUsuario from './views/CadastroUsuario'
 import Login from './views/Login'
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <CadastroUsuario />
     </div>
   );
 }
